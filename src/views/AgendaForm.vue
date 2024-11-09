424c238e988d12af2ba62db52237ec53fec5f470
Luka Barth <lukadbarth@hotmail.com>
2024-11-09 14:52:35 -0300

    finaliza formulário de agenda

--- src/views/AgendaForm.vue
+++ src/views/AgendaForm.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="form-agendas">
         <form @submit.prevent="salvarAgenda">
+            <h2>Cadastro de Agenda</h2>
             <label for="dia-semana">Escolha um dia da semana:</label>
             <select v-model="agenda.diaSemana" id="dia-semana">
                 <option disabled value="">Selecione um dia</option>
@@ -62,6 +63,12 @@ export default {
         },
 
         async salvarAgenda() {
+            
+            if (!this.agenda.diaSemana || !this.agenda.horarioDisponivel || !this.agenda.medicoId) {
+                alert("Por favor, preencha todos os campos.");
+                return;
+            }
+
             try {
                 const response = await fetch("http://localhost:8080/agendas", {
                     method: 'POST',
@@ -71,6 +78,7 @@ export default {
 
                 if (response.ok) {
                     alert("Agenda salva com sucesso!");
+                    this.agenda = { diaSemana: null, horarioDisponivel: null, medicoId: null };
                 } else {
                     console.error("Erro ao salvar agenda:", response.statusText);
                 }
@@ -82,4 +90,42 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+html, body {
+  padding-bottom: 100px;
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.form-agendas {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: #280457;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-left: 100px;
+}
+
+input[type="text"], input[type="submit"] {
+  padding: 10px;
+  margin: 10px 0;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+</style>
