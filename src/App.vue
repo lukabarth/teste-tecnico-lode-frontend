02db48423648166eb24862d8b951abad10bbdbaf
Luka Barth <lukadbarth@hotmail.com>
2024-11-08 23:10:16 -0300

    adiciona os templates no arquivo principal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
+<script setup>
+import MedicoForm from './components/MedicoForm.vue';
+import PacienteForm from './components/PacienteForm.vue';
+import AgendaForm from './components/AgendaForm.vue';
+</script>
+
 <template>
+<MedicoForm></MedicoForm>
+<PacienteForm></PacienteForm>
+<AgendaForm></AgendaForm>
 </template>
 
-<script>
-</script>
 
 <style>
 </style>
